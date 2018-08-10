19c7ca68380927608a133878df47c2d436c1db6b
Kevin Jared <kevinjaredkip@gmail.com>
2018-08-10 12:40:53 +0300

    setup files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteUpvoteComponent } from './quote-upvote/quote-upvote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteUpvoteComponent
   ],
   imports: [
     BrowserModule

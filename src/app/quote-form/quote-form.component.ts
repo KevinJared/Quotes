8fcf18950eb285b6eb7ad860361522648e758480
Kevin Jared <kevinjaredkip@gmail.com>
2018-08-12 16:16:21 +0300

    ng model  with post button

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  quoteDetails: string;
+  newQuote: any;
+  @Output() emitQuote = new EventEmitter();
 
+  submitQuote() {
+    this.newQuote = new Quote(this.quoteDetails);
+    this.emitQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {
